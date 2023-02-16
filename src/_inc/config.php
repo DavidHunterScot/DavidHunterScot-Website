c2a2efdbfcf433bf37c017957847289a99855fb2
DavidHunterScot <david.hunter@echoverse.cc>
2023-02-16 12:57:38 +0000

    Fixed duplicate CSS and JS loading issue.

--- src/_inc/config.php
+++ src/_inc/config.php
@@ -13,9 +13,11 @@ $avatars_url = $images_url . "/avatars";
 
 $webfonts_url = "/assets/webfonts";
 
+$stylesheets = array();
 $stylesheets[] = $webfonts_url . "/poppins/poppins.css";
 $stylesheets[] = "/assets/css/stylesheet.css";
 
+$footer_javascripts = array();
 $footer_javascripts[] = "/assets/js/anti-email-harvesting.js";
 $footer_javascripts[] = "/assets/js/toggle-color-scheme.js";
 
