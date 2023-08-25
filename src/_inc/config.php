b030c65db0cec94ac45a695f0fa0086788a11d61
DavidHunterScot <david.hunter@echoverse.cc>
2023-08-25 17:45:14 +0100

    Changed to using Google Fonts for Poppins WebFont.

--- src/_inc/config.php
+++ src/_inc/config.php
@@ -13,9 +13,7 @@ $avatars_url = $images_url . "/avatars";
 
 $videos_url = "https://videos.staticly.ict.rocks";
 
-$webfonts_url = "https://webfonts.staticly.ict.rocks";
-
-$stylesheets['poppins_webfont_stylesheet'] = $webfonts_url . "/poppins/poppins.css";
+$stylesheets['poppins_webfont_stylesheet'] = "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap";
 $stylesheets['stylesheet'] = "/assets/css/stylesheet.css";
 
 $footer_javascripts['anti-email-harvesting'] = "/assets/js/anti-email-harvesting.js";
