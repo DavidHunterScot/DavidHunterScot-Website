86c34fb6a5deda5b08eb6af73925ecc2583ae3a6
DavidHunterScot <david.hunter@echoverse.cc>
2023-12-12 20:45:22 +0000

    Updated video URLs to staticly echoverse variants

--- src/_inc/base.php
+++ src/_inc/base.php
@@ -6,7 +6,7 @@ site_author: David Hunter
 website_source_code_url: https://github.com/DavidHunterScot/Website
 
 images_url: /assets/images
-videos_url: https://videos.staticly.cc
+videos_url: https://videos.staticly.echoverse.cc
 webfonts_url: /assets/webfonts
 
 content_placeholder: {{ content }}
