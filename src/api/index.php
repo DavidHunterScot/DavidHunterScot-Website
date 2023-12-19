2d1703f242282b931e1c1f24647fc9ff708508db
DavidHunterScot <david.hunter@echoverse.cc>
2023-12-19 22:06:33 +0000

    Added API Documentation

---
+++ src/api/index.php
@@ -0,0 +1,22 @@
+---
+page_title: API Documentation
+layout: src/_inc/base.php
+---
+
+<h2>API Documentation</h2>
+
+<p>Welcome to the API Documentation. Here you can find out how to remotely access information provided by this website.</p>
+
+<h3>Blog Entries</h3>
+
+<p>Get all blog entries.</p>
+
+<h4><b>REQUEST</b></h4>
+
+<p><code><b>GET:</b> /api/blog/entries.json</code></p>
+
+<p></p>
+
+<h4><b>RESPONSE</b></h4>
+
+<p>The response will be in <code>application/json</code> format. <a href="/api/blog/entries.json" target="_blank">View Response</a>.</p>
