21737b6c904b9d7072c4b58a74c3c255dbf614f4
DavidHunterScot <david.hunter@echoverse.cc>
2024-09-18 20:21:35 +0100

    Reinstated the David Hunter website as a standalone website.

---
+++ src/blog/2024/05/17/server-management-tutorials.php
@@ -0,0 +1,20 @@
+---
+article_title: Server Management Tutorials
+article_description: Learn how to manage your servers using these tutorials.
+article_author: David Hunter
+article_date: 2024-05-17
+article_time: 14:15
+layout: src/_inc/base.php
+current_page: blog
+---
+
+<p>--- metadata.article_description ---</p>
+
+<p>Debian-Based Linux Servers</p>
+
+<ul>
+    <li><a href="/assets/text/Secure-and-Manage-a-Debian-Based-Linux-Server-Tutorial.txt" target="_blank">SECURE AND MANAGE A DEBIAN-BASED LINUX SERVER - TUTORIAL</a></li>
+    <li><a href="/assets/text/Debian-Based-Linux-Web-Server-using-Apache-Tutorial.txt" target="_blank">DEBIAN-BASED LINUX WEB SERVER USING APACHE - TUTORIAL</a></li>
+</ul>
+
+<p>I do hope these tutorials have been useful to you. Feel free to provide me with feedback. :)</p>
--- src/projects.php
+++ src/projects.php
@@ -4,85 +4,86 @@ current_page: projects
 layout: src/_inc/base.php
 ---
 
-    <h1 style="text-transform: uppercase; text-align: center; font-size: 2.3rem;">Projects</h1>
+<h2 style="text-transform: uppercase; text-align: center; font-size: 2.3rem;">Projects</h2>
 
-    <?php
+<?php
 
-    $projects = array
+$projects = array
+(
+    array
     (
-        array
-        (
-            "title" => "Personal Website",
-            "description" => "It is all about me",
-            "url" => "https://davidhunter.scot",
-            "image" => "--- metadata.images_url ---/avatars/davidhunter_illustration_transparent_1.png",
-            "color" => "#333",
-        ),
-        array
-        (
-            "title" => "StaticPHP",
-            "description" => "A Static Site Generator that turns PHP files into HTML files.",
-            "url" => "https://staticphp.io",
-            "image" => "--- metadata.images_url ---/logos/StaticPHP-Promo1.png",
-            "color" => "#7A86B8",
-        ),
-        array
-        (
-            "title" => "Staticly",
-            "description" => "The essential repository for Front End Web Developers.",
-            "url" => "https://staticly.echoverse.cc",
-            "image" => "",
-            "color" => "#4A9CB3",
-        ),
-        array
-        (
-            "title" => "Network Tools",
-            "description" => "Web based tools related to computer networks.",
-            "url" => "https://networktools.ict.rocks",
-            "image" => "",
-            "color" => "#ffffff",
-        ),
-        array
-        (
-            "title" => "DAH5",
-            "description" => "The website that started it all, originally launched back in April 2005!",
-            "url" => "https://www.dah5.com",
-            "image" => "--- metadata.images_url ---/logos/dah5-logo-2022.svg",
-            "color" => "rgb( 38, 38, 38 )",
-        ),
-    );
-    ?>
+        "title" => "Personal Website",
+        "description" => "It is all about me",
+        "url" => "https://david-hunter.net",
+        "image" => "/assets/images/avatars/davidhunter_illustration_transparent_1.png",
+        "color" => "#333",
+    ),
+    array
+    (
+        "title" => "StaticPHP",
+        "description" => "A Static Site Generator that turns PHP files into HTML files.",
+        "url" => "https://staticphp.io",
+        "target" => "_blank",
+        "image" => "/assets/images/logos/StaticPHP-Promo1.png",
+        "color" => "#7A86B8",
+    ),
+    array
+    (
+        "title" => "Staticly",
+        "description" => "The essential repository for Front End Web Developers.",
+        "url" => "https://www.dah5.me.uk/staticly",
+        "image" => "",
+        "color" => "#4A9CB3",
+    ),
+    array
+    (
+        "title" => "Network Tools",
+        "description" => "Web based tools related to computer networks.",
+        "url" => "https://networktools.ict.rocks",
+        "target" => "_blank",
+        "image" => "",
+        "color" => "#ffffff",
+    ),
+    array
+    (
+        "title" => "DAH5",
+        "description" => "The website that started it all, originally launched back in April 2005!",
+        "url" => "https://www.dah5.me.uk",
+        "image" => "/assets/images/logos/dah5-logo-2024-alternate-150x35.png",
+        "color" => "rgb( 38, 38, 38 )",
+    ),
+);
+?>
 
-    <section class="projects">
-        <div class="container">
+<section class="projects">
+    <div class="container">
+        <?php
 
-            <?php
+        for( $p = 0; $p < count( $projects ); $p++ )
+        {
+            $project = $projects[ $p ];
+        ?>
 
-            for( $p = 0; $p < count( $projects ); $p++ )
-            {
-                $project = $projects[ $p ];
-            ?>
-
-            <div class="project">
-                <?php if( $p % 2 === 0 ): ?>
-                    <div class="image left hide-mobile" style="background-image: url( '<?php echo $project['image']; ?>' ), linear-gradient( var( --angle-alt ), <?php if( isset( $project['color'] ) && $project['color'] ) echo $project['color']; else echo "#262626"; ?> 0%, var( --color-background-alt ) 50% );">&nbsp;</div>
-                <?php endif; ?>
-                <div class="info">
-                    <div class="title"><?php echo $project['title']; ?></div>
-                    <div class="description"><?php echo $project['description']; ?></div>
-                    <div class="url"><a href="<?php echo $project['url']; ?>" target="_blank"><?php echo $project['url']; ?></a></div>
-                </div>
-                <?php if( $p % 2 !== 0 ): ?>
-                    <div class="image right" style="background-image: url( '<?php echo $project['image']; ?>' ), linear-gradient( var( --angle ), <?php if( isset( $project['color'] ) && $project['color'] ) echo $project['color']; else echo "#262626"; ?> 0%, var( --color-background-alt ) 50% );">&nbsp;</div>
-                <?php else: ?>
-                    <div class="image hide-desktop" style="background-image: url( '<?php echo $project['image']; ?>' ), linear-gradient( var( --angle ), <?php if( isset( $project['color'] ) && $project['color'] ) echo $project['color']; else echo "#262626"; ?> 0%, var( --color-background-alt ) 50% );">&nbsp;</div>
-                <?php endif; ?>
+        <div class="project">
+            <?php if( $p % 2 === 0 ): ?>
+                <div class="image left hide-mobile" style="background-image: url( '<?php echo $project['image']; ?>' ), linear-gradient( var( --angle-alt ), <?php if( isset( $project['color'] ) && $project['color'] ) echo $project['color']; else echo "#262626"; ?> 0%, var( --color-background-alt ) 50% );">&nbsp;</div>
+            <?php endif; ?>
+            <div class="info">
+                <div class="title"><?php echo $project['title']; ?></div>
+                <div class="description"><?php echo $project['description']; ?></div>
+                <div class="url"><a href="<?php echo $project['url']; ?>"<?php if( isset( $project['target'] ) ) echo ' target="' . $project['target'] . '"'; ?>><?php echo $project['url']; ?></a></div>
             </div>
-
-            <?php
-            
-            }
-
-            ?>
+            <?php if( $p % 2 !== 0 ): ?>
+                <div class="image right" style="background-image: url( '<?php echo $project['image']; ?>' ), linear-gradient( var( --angle ), <?php if( isset( $project['color'] ) && $project['color'] ) echo $project['color']; else echo "#262626"; ?> 0%, var( --color-background-alt ) 50% );">&nbsp;</div>
+            <?php else: ?>
+                <div class="image hide-desktop" style="background-image: url( '<?php echo $project['image']; ?>' ), linear-gradient( var( --angle ), <?php if( isset( $project['color'] ) && $project['color'] ) echo $project['color']; else echo "#262626"; ?> 0%, var( --color-background-alt ) 50% );">&nbsp;</div>
+            <?php endif; ?>
         </div>
-    </section>
+
+        <?php
+        
+        }
+
+        ?>
+    </div>
+</section>
